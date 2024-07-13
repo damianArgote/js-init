3fab2ba88993a7a664650d7f0ef6072a4d8e4ed5
Pablo Campos <pabloezequielc@gmail.com>
2024-07-13 03:52:33 -0300

    primer tarea

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 
 //objetos key(llave) : value (valor)
 const producto = {
-    nombre:'notebook',
-    id:1,
-    descripcion:'Lenovo usado con garantia',
-    precio:2500000,
-    disponible:true,
-    categorias:['Usado','Notebook','Garantia'],
-    informacion:{
-        marca:'Lenovo',
-        pais:'Argentina',
-        numeroSerie:12345678
+    nombre: 'notebook',
+    id: 1,
+    descripcion: 'Lenovo usado con garantia',
+    precio: 2500000,
+    disponible: true,
+    categorias: ['Usado', 'Notebook', 'Garantia'],
+    informacion: {
+        marca: 'Lenovo',
+        pais: 'Argentina',
+        numeroSerie: 12345678
     }
 }
 
@@ -34,28 +34,30 @@ const producto = {
 
 const listadoUl = document.querySelector('#listado');
 
-function imprimirObjeto(){
-    
-    for(let key in producto){
+function imprimirObjeto() {
+
+    for (let key in producto) {
         const li = document.createElement('li');//creamos el elemento li
 
         //validar por el key informacion
-        if( key === 'informacion'){
-           //Se vuelve a interar el objeto producto.informacion
-            for(let llave in producto.informacion){
+        if (key === 'informacion') {
+            //Se vuelve a interar el objeto producto.informacion
+            for (let llave in producto.informacion) {
                 const div = document.createElement('div');
-                div.innerHTML=`<ul>
+                div.innerHTML = `<ul>
                     <li>${producto.informacion[llave]}</li>
                 </ul>`
-                
+
                 li.appendChild(div);
             }
-            
-        }else{
-            li.innerHTML=`<strong>${key}</strong>: <span>${producto[key]}</span>`; //le damo el valor al elemento
+
+        } else {
+            li.innerHTML = `<strong>${key}</strong>: <span>${producto[key]}</span>`; //le damo el valor al elemento
         }
         listadoUl.appendChild(li); //unimos el elemento al padre ul
     }
 }
 
 imprimirObjeto()
+
+console.log('hola soy pablo');
