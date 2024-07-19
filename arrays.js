341bc2cdac47a62d61c265df05d26103b5c6591d
Damián Argote <DArgote@coasa.com.ar>
2024-07-19 17:37:46 -0300

    Clase 2 arrays

---
+++ arrays.js
@@ -0,0 +1,59 @@
+//Arrays declaraciones y metodos
+
+const numeros = [1,50,10,4,100,2000]
+const booleanos = [true,false,false,true]
+const arrayVariado = [12,false,'hola mundo',{mensaje:'esto es un mensaje'}];
+//Mas comunes
+const meses = ['Enero','Febrero','Marzo','Abril']
+const tecnologias =[
+    {
+        framework:'React',
+        stack:'Frontend',
+        logo:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMbJS-A8NEllfqbKf-4GUKetJe8J3GKqKP6A&s',
+        id:1
+    },
+    {
+        framework:'Angular',
+        stack:'Frontend',
+        logo:'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png',
+        id:2
+    },
+    {
+        framework:'Nodejs',
+        stack:'Backend',
+        logo:'https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg',
+        id:3
+    },
+    {
+        framework:'Nestjs',
+        stack:'Backend',
+        logo:'https://miro.medium.com/v2/resize:fit:1358/1*s9kgU8F1eB7Tzs7sG0YhBg.jpeg',
+        id:4
+    },
+    {
+        framework:'.NET',
+        stack:'Backend',
+        logo:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvRYzE6vnRTPpJK4DV0v8Af0WyiuTteVj9cg&s',
+        id:5
+    },
+    {
+        framework:'Nextjs',
+        stack:'Frontend',
+        logo:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxpA-HSZ8PwRVGSnKwXNtbm-f3ZB_tc6Kdhg&s',
+        id:6
+    },
+    {
+        framework:'MySQL',
+        stack:'Database',
+        logo:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl3P-jnVaFvLyC8AweZzJ9vQY6Y4egFM2tLQ&s',
+        id:7
+    },
+    {
+        framework:'MongoDb',
+        stack:'Database',
+        logo:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYP2wh48_6wrG4tZleiAngKQ0ThkCjuKaSzw&s',
+        id:8
+    }
+]
+
+console.log(tecnologias);
